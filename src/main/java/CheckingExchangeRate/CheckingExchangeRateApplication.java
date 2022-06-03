d7f0405126a6c9e039d68694ad5158df42e8e96b
Алиса Аниськина <alisa.aniskina@gmail.com>
2022-06-03 11:22:09 +0600

    Создание проекта

---
+++ src/main/java/CheckingExchangeRate/CheckingExchangeRateApplication.java
@@ -0,0 +1,13 @@
+package CheckingExchangeRate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckingExchangeRateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckingExchangeRateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/CheckingExchangeRate/CheckingExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package CheckingExchangeRate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckingExchangeRateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
